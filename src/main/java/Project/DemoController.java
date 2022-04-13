fa1e117836b07c16d8970dc93d7ae8de2f03a32d
KishoreV@92 <kishorevjn835@gmail.com>
2022-04-13 17:34:07 +0530

    APril 13

--- src/main/java/Project/DemoController.java
+++ src/main/java/Project/DemoController.java
@@ -1,8 +1,6 @@
 package Project;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
@@ -11,9 +9,5 @@ public class DemoController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Demo";
-	}
-	@PostMapping("/login")
-	public void login(@RequestBody User user) {
-		System.out.println(user.getUserName() + ":" + user.getPassword());
 	}
 }
